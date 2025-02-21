75dc730788647402008f2a14d2b28242140a5f98
aayush-619 <rajbharaayush03@gmail.com>
2025-02-21 19:55:56 +0530

    added login page and updated LoginTest correspondingly

--- src/test/java/demo_test/LoginTest.java
+++ src/test/java/demo_test/LoginTest.java
@@ -1,32 +1,40 @@
 package demo_test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-	WebDriver webDriver;
-	
-	@BeforeTest
-	public void setup() {
-		System.setProperty("webdriver.chrome.driver","path/to/chromedriver");
-		webDriver = new ChromeDriver();
-		
-		webDriver.get("https://example.com/login");
-	}
-	
-	
-	@Test
-	public void testLogin() {
-		System.out.println("Executing... login test");
-	}
-	
-	
-	@AfterTest
-	public void teardown() {
-		webDriver.quit();
-	}
+    WebDriver driver;
 
+    @BeforeTest
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver"); // Update with correct path
+        driver = new ChromeDriver();
+        driver.get("file:///C:/Users/YourName/path-to-project/login.html"); // Change to the actual file path
+    }
+
+    @Test
+    public void testValidLogin() {
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        username.sendKeys("testuser");
+        password.sendKeys("password123");
+        loginButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        Assert.assertEquals(message.getText(), "Login successful!");
+    }
+
+    @AfterTest
+    public void teardown() {
+        driver.quit();
+    }
 }
